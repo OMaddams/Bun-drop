59d909b4b622cd2fca6c4efe3cee254c5b7a688b
OMaddams <Omaddams@hotmail.com>
2024-05-22 10:25:00 +0200

    hover and click effects on showcase

--- src/Components/ShowcaseItem.jsx
+++ src/Components/ShowcaseItem.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
+
 function ShowcaseItem({ data }) {
+  const [hide, setHide] = useState("hide");
+
+  const onHover = () => {
+    setHide("");
+  };
+  const onLeave = () => {
+    setHide("hide");
+  };
+  const flipHide = () => {
+    hide === "hide" ? setHide("") : setHide("hide");
+  };
   return (
-    <div className="showcase-item">
+    <div
+      className="showcase-item"
+      onMouseEnter={onHover}
+      onMouseLeave={onLeave}
+      onClick={flipHide}
+    >
       <img
         className="showcase-item-img"
         src={`${process.env.PUBLIC_URL}/img/${data.image}`}
       ></img>
-      <p>{data.title}</p>
+      <div className={`showcase-item-text ${hide}`}>
+        <p>{data.title}</p>
+      </div>
     </div>
   );
 }
