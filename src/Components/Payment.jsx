f27e18c328d59d401a659a680490f1390442b249
OMaddams <Omaddams@hotmail.com>
2024-05-25 21:55:56 +0200

    almost done with card info, last bit of validation kicking my ass

--- src/Components/Payment.jsx
+++ src/Components/Payment.jsx
@@ -2,7 +2,65 @@ import { useState } from "react";
 
 function Payment() {
   const [selectedMethod, setSelectedMethod] = useState();
+  const [cardNumber, setCardNumber] = useState("");
+  const [cardNumError, setCardNumError] = useState("");
+  const [cvc, setCvc] = useState("");
+  const [month, setMonth] = useState();
+  const [year, setYear] = useState("");
+  console.log(month);
+  function cardNumOnChange(e) {
+    let cardErrorText = "";
 
+    if (e.target.value.trim().length < 8) {
+      cardErrorText += "Card number too short \n";
+    }
+    if (e.target.value.trim().length > 16) {
+      cardErrorText += "Card number too long\n";
+    }
+    let tidyNum = e.target.value.replace(/\D/g, "");
+    setCardNumError(cardErrorText);
+    setCardNumber(tidyNum.trim());
+  }
+  function cvcOnChange(e) {
+    let tidyNum = e.target.value.replace(/\D/g, "");
+
+    if (tidyNum.length > 3) {
+      return;
+    }
+    setCvc(tidyNum.trim());
+  }
+  function monthOnChange(e) {
+    let tidyNum = e.target.value.replace(/\D/g, "");
+    if (tidyNum === "") {
+      setMonth("");
+    } else if (tidyNum < 1) {
+      setMonth(1);
+    } else if (tidyNum > 12) {
+      setMonth(12);
+    } else {
+      setMonth(tidyNum);
+    }
+
+    preventExpired();
+  }
+  function yearOnChange(e) {
+    let tidyNum = e.target.value.replace(/\D/g, "");
+    if (tidyNum === "") {
+      setYear("");
+    } else if (tidyNum > currentYearLastTwo + 12) {
+      setYear(currentYearLastTwo + 12);
+    } else {
+      setYear(tidyNum);
+    }
+  }
+  function preventExpired() {
+    if (currentYearLastTwo > year) {
+      setYear(currentYearLastTwo);
+    }
+    if (currentYearLastTwo == year && currentMonth > month) {
+      setMonth(currentMonth);
+    }
+  }
   const swishForm = () => {
     return (
       <>
@@ -14,38 +72,76 @@ function Payment() {
   const onFocusOut = (type) => {
     if (type === "month") {
     }
+    if (type === "year") {
+    }
   };
   let currentTime = new Date();
+  const currentYearLastTwo = currentTime.getFullYear() - 2000;
+  const currentMonth = currentTime.getMonth() + 1;
+  console.log(currentTime.getMonth());
   const cardForm = () => {
     return (
       <div>
         <p>Card</p>
         <form id="payment" className="shipping-container">
           <label htmlFor="">Card number</label>
-          <input type="text" name="" id="" className="shipping-inputs" />
-          <label htmlFor="">CVC</label>
-          <input type="text" name="" id="" className="shipping-inputs" />
-          <label htmlFor="">Expiration</label>
           <input
-            type="number"
-            min={1}
-            max={12}
+            type="text"
             name=""
             id=""
             className="shipping-inputs"
-            onBlur={onFocusOut("month")}
+            value={cardNumber}
+            onChange={cardNumOnChange}
           />
-          <input
-            className="shipping-inputs"
-            type="number"
-            placeholder={currentTime.getFullYear()}
-            min={currentTime.getFullYear()}
-            max={currentTime.getFullYear() + 10}
-            onBlur={onFocusOut("month")}
-            name=""
-            id=""
-          />
+          <div className="payment-card-details">
+            <div className="payment-card-input">
+              <label htmlFor="">CVC</label>
+              <input
+                type="text"
+                name=""
+                id=""
+                className="shipping-inputs"
+                value={cvc}
+                onChange={cvcOnChange}
+              />
+            </div>
+            <div className="payment-card-input">
+              <label htmlFor="">Expiration</label>
+              <div style={{ display: "flex" }}>
+                <input
+                  placeholder={currentMonth}
+                  value={month}
+                  onChange={monthOnChange}
+                  type="text"
+                  min={1}
+                  max={12}
+                  name=""
+                  id=""
+                  className="shipping-inputs payment-card-expiration"
+                  onBlur={onFocusOut("month")}
+                />
+                <p className="margin-15"> / </p>
+                <input
+                  className="shipping-inputs payment-card-expiration"
+                  type="text"
+                  value={year}
+                  onChange={yearOnChange}
+                  placeholder={currentYearLastTwo}
+                  min={currentYearLastTwo}
+                  max={currentYearLastTwo + 10}
+                  onBlur={onFocusOut("year")}
+                  name=""
+                  id=""
+                />
+              </div>
+            </div>
+          </div>
         </form>
+        <div className="validation-text">
+          <p className="validation-text">{cardNumError}</p>
+          <p className="validation-text">error</p>
+          <p className="validation-text">error</p>
+        </div>
       </div>
     );
   };
